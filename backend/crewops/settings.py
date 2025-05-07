7e40c51458a822c92efadcfa65e2445246f07877
Jaspal Brah <jas@Jaspals-MacBook-Air.local>
2025-05-07 16:51:48 -0600

    Configure PostgreSQL DATABASE_URL for Railway

--- backend/crewops/settings.py
+++ backend/crewops/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'crewops.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
+    'default': dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
